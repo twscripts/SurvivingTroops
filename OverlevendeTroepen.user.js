107f5885f13833ed40a81a74ff604f14512190b7
Kevin Coorens <kevin.coorens@student.kuleuven.be>
2016-01-27 22:52:16 +0100

    ODD & ODA added!

--- OverlevendeTroepen.user.js
+++ OverlevendeTroepen.user.js
@@ -2,57 +2,92 @@
 // @name        Overlevende Troepen
 // @namespace   kc-productions.org
 // @include     https://*.tribalwars.*/game.php?*&screen=report*
-// @version     1.2
+// @version     1.3
 // @grant       none
 // ==/UserScript==
 
 function survivors(table, deff){
   var row = table.insertRow(table.rows.length);
-  // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+
   if(deff==1){
     for(var c = 0; c<13; c++){
-      row.insertCell(c)
+      row.insertCell(c);
+      var doden = new Array(13);
     }
   }
   else{
     for(var c = 0; c<12; c++){
-      row.insertCell(c)
+      row.insertCell(c);
+      var doden = new Array(12);
     }
   }
 
+  var aantal = table.rows[table.rows.length-3];
+  var gestorven = table.rows[table.rows.length-2];
 
-  var aanval = new Array();
-  var doden = new Array();
-  var overlevend = new Array();
-  var row2 = table.rows[table.rows.length-3];
-  for (var j = 1, col; col = row2.cells[j]; j++) {
-       aanval.push(col.innerHTML);
-     }
-   row2 = table.rows[table.rows.length-2];
-   for (var j = 1, col; col = row2.cells[j]; j++) {
-        doden.push(col.innerHTML);
-      }
-  for(var i=0; i<doden.length;i++){
-    overlevend[i]=aanval[i]-doden[i];
-  }
-  var k = 0;
   for (var j = 0, col; col = row.cells[j]; j++) {
-       col.innerHTML= overlevend[k];
-       if(overlevend[k]!=0){
+    doden[j]=gestorven.cells[j+1].innerHTML;
+    var survived = aantal.cells[j+1].innerHTML-gestorven.cells[j+1].innerHTML;
+       col.innerHTML= survived;
+       if(survived!=0){
          col.className = "unit-item unit-item-axe" ;
        }
        else{
          col.className = "unit-item unit-item-sword hidden";
        }
        col.style = "text-align:center";
-       k++;
      }
    var temp = row.insertCell(0);
    temp.innerHTML = "Overlevend:";
    temp.style.width = "20%";
+   return doden;
 }
 
-var table = document.getElementById("attack_info_att_units");
-survivors(table, 0);
-table = document.getElementById("attack_info_def_units");
-survivors(table,1);
+function calcODA(array, table){
+  var result = 0;
+  result+=4*array[0];
+  result+=5*array[1];
+  result+=1*array[2];
+  result+=5*array[3];
+  result+=1*array[4];
+  result+=5*array[5];
+  result+=6*array[6];
+  result+=23*array[7];
+  result+=4*array[8];
+  result+=12*array[9];
+  result+=40*array[10];
+  result+=200*array[11];
+
+  var row = table.insertRow(table.rows.length);
+  var cell1 = row.insertCell(0);
+  cell1.innerHTML = "ODA: " + result;
+  cell1.style.fontWeight = 'bold';
+
+}
+function calcODD(array, table){
+  var result = 0;
+  result+=1*array[0];
+  result+=2*array[1];
+  result+=4*array[2];
+  result+=2*array[3];
+  result+=2*array[4];
+  result+=13*array[5];
+  result+=12*array[6];
+  result+=15*array[7];
+  result+=8*array[8];
+  result+=10*array[9];
+  result+=20*array[10];
+  result+=200*array[11];
+
+  var row = table.insertRow(table.rows.length);
+  var cell1 = row.insertCell(0);
+  cell1.innerHTML = "ODD: " + result;
+  cell1.style.fontWeight = 'bold';
+}
+
+var attack = document.getElementById("attack_info_att_units");
+var deathByDefense = survivors(attack, 0);
+var defense = document.getElementById("attack_info_def_units");
+var deathByOffense = survivors(defense,1);
+calcODA(deathByOffense, attack);
+calcODD(deathByDefense, defense);

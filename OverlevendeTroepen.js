6c3c14d07cfbdd1cc2afc985af2f7490aab2364e
Kevin Coorens <kevin.coorens@student.kuleuven.be>
2016-01-27 21:14:35 +0100

    Create OverlevendeTroepen.js

---
+++ OverlevendeTroepen.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name        Overlevende Troepen
+// @namespace   kc-productions.org
+// @include     https://*.tribalwars.*/game.php?*&screen=report*
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+function survivors(table){
+  var row = table.insertRow(table.rows.length);
+  // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+  for(var c = 0; c<12; c++){
+    row.insertCell(c)
+  }
+
+
+  var aanval = new Array();
+  var doden = new Array();
+  var overlevend = new Array();
+  var row2 = table.rows[table.rows.length-3];
+  for (var j = 1, col; col = row2.cells[j]; j++) {
+       aanval.push(col.innerHTML);
+     }
+   row2 = table.rows[table.rows.length-2];
+   for (var j = 1, col; col = row2.cells[j]; j++) {
+        doden.push(col.innerHTML);
+      }
+  for(var i=0; i<doden.length;i++){
+    overlevend[i]=aanval[i]-doden[i];
+  }
+  var k = 0;
+  for (var j = 0, col; col = row.cells[j]; j++) {
+       col.innerHTML= overlevend[k];
+       if(overlevend[k]!=0){
+         col.className = "unit-item unit-item-axe" ;
+       }
+       else{
+         col.className = "unit-item unit-item-sword hidden";
+       }
+       col.style = "text-align:center";
+       k++;
+     }
+   var temp = row.insertCell(0);
+   temp.innerHTML = "Overlevend:";
+   temp.style.width = "20%";
+}
+
+var table = document.getElementById("attack_info_att_units");
+survivors(table);
+table = document.getElementById("attack_info_def_units");
+survivors(table);

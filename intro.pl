8ac0188e079c3164cd8a15ed1ea9a273c669a9b2
Mathias Dekempeneer <mdekempeneer@users.noreply.github.com>
2016-10-11 13:53:49 +0200

    Update intro.pl
    Added some functions

--- intro.pl
+++ intro.pl
@@ -15,3 +15,25 @@ sibling(X,Y) :-
     mother(M,X),
     mother(M,Y),
     X \== Y.
+
+ancestor(X,Y) :- father(X,Y).
+
+ancestor(X,Y) :-
+    father(Z,Y),
+    ancestor(X,Z).
+    
+
+peano_plus(zero,X,X).
+
+peano_plus(s(X),Y,s(Z)) :- peano_plus(X,Y,Z).
+
+min(X,zero,X).
+
+min(s(X),s(Y),Z) :- min(X,Y,Z).
+
+greater_than(s(_),zero).
+greater_than(s(X),s(Y)) :- greater_than(X,Y).
+
+maximum(X,X,X).
+maximum(X,Y,X) :- greater_than(X,Y).
+maximum(X,Y,Y) :- greater_than(Y,X).
